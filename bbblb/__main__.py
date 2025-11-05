19fca62fc803fe278d3d1c588350543682707050
Marcel Hellkamp <marc@gsites.de>
2025-11-05 16:47:52 +0100

    Start building an actual command-line interface.

--- bbblb/__main__.py
+++ bbblb/__main__.py
@@ -3,21 +3,6 @@ import sys
 import time
 import jwt
 
-from bbblb.settings import config
+from bbblb.cli import main
 
-config.populate()
-
-
-def cmd_maketoken(subject, maxage, *scopes):
-    payload = {
-        "sub": subject,
-        "exp": int(time.time() + int(maxage)),
-        "scope": " ".join(scopes),
-        "jti": secrets.token_hex(8),
-    }
-    print(payload, file=sys.stderr)
-    print(jwt.encode(payload, config.SECRET))
-
-
-cmd = sys.argv[1]
-locals()[f"cmd_{cmd}"](*sys.argv[2:])
+main()
