9b15e00a08a341874c3aef34fab3963dc0e70ba4
Marcel Hellkamp <marc@gsites.de>
2025-11-14 07:51:40 +0100

    Release of v0.0.6
    Changes:
    
    - b9649a2 Improved Caddyfile example
    - f099551 Optimized poller for high server counts
    - e6353f4 fix: 500 during getMeetings
    - 42bafe1 fix: Missing BBBResponse.error property
    - bedb87b fix: _ElementTree truth check warning
    - b182988 fix: dictionary changed size during iteration
    - 7b2dbd6 Log back-end response status code on error
    - 24c8a18 fix: Poller ignores certain errors
    - 5dd7792 Reduce loglevel of poller success message
    - 11213a5 Fix 'Unclosed client session' warnings
    - 54a577f Fix deprecation warnings from lxml
    - 5f31c7d fix: Do not fail for multiple available servers
    - 6e3fcbd fix: Recursion error when accessing BBB response XML fields via attributes
    - 88580f1 Create database if it does not exist
    - 6b2f34c Added --wait parameter to "bbblb server disable"
    - 63b89da Github wants the LICENSE file without qualifier
    - 65c1803 Highlight project state in main README
    - 7229827 Documentation
    - daa64d2 No special treatment for the the  'default' tenant.

--- bbblb/__init__.py
+++ bbblb/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from .settings import config
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 VERSION = __version__.split(".", 2)
 VERSION[-1], _, BUILD = VERSION[-1].partition("-")
 
