bb2b18f764086a646f1abdef2ff1c269b508585f
Marcel Hellkamp <marc@gsites.de>
2025-11-15 10:23:02 +0100

    Work on example upload script
    Retry up to 10 times with growing delays

--- examples/post_publish_bbblb.rb
+++ examples/post_publish_bbblb.rb
@@ -16,23 +16,24 @@ BigBlueButton.logger = logger
 opts = Optimist::options do
   opt :meeting_id, "Meeting id to archive", :type => String
   opt :format, "Playback format name", :type => String
+  opt :origin, "Override bbblb-origin metadata and upload to a different bbblb instance", :type => String
+  opt :tenant, "Override bbblb-tenant metadata with a different tenant", :type => String
 end
 meeting_id = opts[:meeting_id]
 format_name = opts[:format]
+override_origin = opts[:origin]
+override_tenant = opts[:tenant]
 raise('Missing parameter: --meeting-id') unless meeting_id
 
-# Load BBB server secret
+# Load BBB server secret and domain
 props = JavaProperties::Properties.new("/etc/bigbluebutton/bbb-web.properties")
-secret = props[:securitySalt]
+bbb_secret = props["securitySalt"]
+bbb_host = URI.parse(props["bigbluebutton.web.serverURL"]).host
 
 # Load recording paths
 props = YAML.safe_load(File.open('../../core/scripts/bigbluebutton.yml'))
 published_dir = props['published_dir'] || raise('Unable to determine published_dir from bigbluebutton.yml')
 recording_dir = props['recording_dir'] || raise('Unable to determine recording_dir from bigbluebutton.yml')
-
-# Load meeting metadata
-events_xml = "#{recording_dir}/raw/#{meeting_id}/events.xml"
-metadata = BigBlueButton::Events.get_meeting_metadata(events_xml)
 
 # Find published recording directories
 if format_name then
@@ -44,22 +45,42 @@ end
 format_dirs = format_dirs.filter(File.directory?)
 raise("No matching recordings found") unless format_dirs.empty?
 
+
+# Load meeting metadata
+events_xml = "#{recording_dir}/raw/#{meeting_id}/events.xml"
+metadata = BigBlueButton::Events.get_meeting_metadata(events_xml)
+
 # Check if the meeting was started by BBBLB
-bbblb_server = metadata['bbblb-server']
-bbblb_origin = metadata['bbblb-origin']
-unless bbblb_server && bbblb_origin then
-  logger.warn("Meeting #{meeting_id} was not started with bbblb, skipping... ")
+meta_origin = metadata['bbblb-origin'].to_s
+meta_origin = override_origin unless override_origin.to_s.empty?
+meta_tenant = metadata['bbblb-tenant'].to_s
+meta_tenant = override_tenant unless override_tenant.to_s.empty?
+
+# Skip meetings not started via bbblb or overridden with --origin and --tenant cli arguments.
+if meta_tenant.empty? || meta_origin.empty? then
+  logger.warn("Skipping #{meeting_id}: Missing bbblb-tenant or bbblb-origin metadata."\
+  " The meeting was probably not started with bbblb.")
   exit 0
 end
 
-# Generate short-time security token for API request
-bbblb_api = "https://#{bbblb_origin}/bbblb/api/v1/recordings/upload"
-bbblb_token = jwt.encode({sub: bbblb_server, exp: Time.now.to_i + 600}, secret, 'HS256', {kid: bbblb_server})
+# Generate API URL and short-time access token signed with server secret
+q = URI.encode_www_form("tenant" => override_tenant) unless override_tenant.to_s.empty?
+upload_url = URI::HTTP.new("https", nil, meta_origin, nil, nil, "/bbblb/api/v1/recordings/upload", nil, q, nil)
+upload_token = jwt.encode({sub: bbb_host, exp: Time.now.to_i + 600}, bbb_secret, 'HS256', {kid: bbb_host})
 
 format_dirs.each do |upload_dir|
-  # TODO: Create the tar archive first, then try to upload it multiple times
-  system(
-    {'UPLOAD'=>upload_dir, 'TOKEN'=>bbblb_token, 'API'=>bbblb_api},
-  'tar -cz "$UPLOAD" | curl --fail-with-body -X POST -T- -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/x-tar" "$API"'
-  ) || raise('Failed to upload recording archive')
+  retries = 10
+  success = False
+  env = {'UPLOAD'=>upload_dir, 'TOKEN'=>upload_token, 'API'=>upload_url}
+  cmd = 'tar -cz "$UPLOAD" | curl -X POST -T- "$API"'\
+        ' --fail-with-body' \
+        ' -H "Authorization: Bearer $TOKEN"' \
+        ' -H "Content-Type: application/x-tar"'
+  retries.times do |i|
+    success = system(env, cmd)
+    break if success
+    logger.warn("Command failed, retrying in #{i + 1} seconds... (Attempt #{i + 1}/#{retries})")
+    sleep(i+1)
+  end
+  raise "Failed to upload recording arcive after #{retries} attempts" unless success
 end
