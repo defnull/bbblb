e31c6f0001203e31235a39855894a6e2be331a9c
Marcel Hellkamp <marc@gsites.de>
2026-04-26 15:03:45 +0200

    License headers

--- bbblb/cli/tenant.py
+++ bbblb/cli/tenant.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2025, 2026  Marcel Hellkamp
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-
 from bbblb import model
 from bbblb.cli.server import _end_meeting
 from bbblb.services import ServiceRegistry
